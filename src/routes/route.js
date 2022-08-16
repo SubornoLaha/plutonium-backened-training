0a30531494e1d98f9555a232d806b6841ed1c848
SubornoLaha <sb.laha45@gmail.com>
2022-08-16 13:29:52 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
+const UserModel = require("../models/userModel.js")
 const UserController= require("../controllers/userController")
+const bookController = require('../controllers/bookController')
+const exda=require('../controllers/expost')
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/createUser", UserController.createUser)
 
 router.get("/getUsersData", UserController.getUsersData)
+
+router.post('/bookpo', bookController.books)
+
+router.get('/bookre', bookController.bookli)
+
+router.post('/su',exda.ex)
+
 
 module.exports = router;
